95ead1bbf8312b1c4dcc33deed69e01e05282315
britbroet <brittanybroetje@gmail.com>
2017-01-19 10:57:19 -0800

    revert back to stormpath only, no omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+# OmniAuth.config.logger = Rails.logger
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, '188748212150-1igv21gv7mpmdr9qp7di1dp0u96rfn86.apps.googleusercontent.com', '4EkCqGYGWzi3IokLfhAQYxXP', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+# end
